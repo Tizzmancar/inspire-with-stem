ed48f7999f11ac373d494b45ec9a0ec182ed3de9
Tizzmancar <carsotheman51@gmail.com>
2022-05-17 12:08:05 +0300

    assignment1.py

--- assignment1.py
+++ assignment1.py
@@ -14,10 +14,34 @@ print(h)
 pi = 3.142
 ba = pi * int(r) * int(r)
 vol = ba * int(h)
-print("volume of cylinder is " str(vol))
+print(" volume of cylinder is " + str(vol))
 
 
 
 #surface area of a cylinder
+r = input(" enter radius of cylinder")
+print(r)
+pi = 3.142
+h = input("enter height of cylinder")
+print("h")
+both = 2
+d = int(r) * both
+curved_surface = d * pi * int(h)
+circle_area = pi * int(r) * int(both)
+surface_area = circle_area + curved_surface
+print("surface area of cylinder is " + str(surface_area))
+
+
+
 #volume of a cube
+l = input("enter length")
+print(l)
+vol = int(l) * int(l) * int(l)
+print("volume of cube is " + str(vol) )
+
+
+
+
+
+
 # let user type input
