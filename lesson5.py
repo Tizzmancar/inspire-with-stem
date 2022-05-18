6a9441fa7c5c750044f04cc3b8190db1343b07d1
Tizzmancar <carsotheman51@gmail.com>
2022-05-18 11:10:26 +0300

    lesson5.py

--- lesson5.py
+++ lesson5.py
@@ -1,1 +1,46 @@
+# methods
+name = "Mathenge Mugo"
+user_name= "Ada Lovelace"
+age = 15
+#person = " I am " + str(name) + " and my age is " + str(age)
+#print(person)
+# the format() method
+#print("My name is {} and I am {} ".format(name, age))
 
+print(f"My name is {name} and I am {age} years old")
+
+#n ->new line
+#t ->tab
+print(f"My \t name is {name} \n and I am")
+
+user_name = "Lovelace"
+print(user_name.strip)
+print(user_name.rstrip)
+print(user_name.lstrip)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+name = "Brett Cooper"
+
+print(name.replace('t','G'))
+
+
+msg = "Hello from Bob Afwata how are you"
+print(msg.split())
+
+print(len(msg))
