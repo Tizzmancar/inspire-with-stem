bbf0d337f18769e49988be9583dca269f2bfc960
Tizzmancar <carsotheman51@gmail.com>
2022-05-18 11:43:37 +0300

    lesson5.py

--- lesson5.py
+++ lesson5.py
@@ -3,6 +3,7 @@ name = "Mathenge Mugo"
 user_name= "Ada Lovelace"
 age = 15
 #person = " I am " + str(name) + " and my age is " + str(age)
+
 #print(person)
 # the format() method
 #print("My name is {} and I am {} ".format(name, age))
@@ -19,22 +20,53 @@ print(user_name.rstrip)
 print(user_name.lstrip)
 
 
+#Multiline strings
+msg= '''WERTYUI56783H Mpesa confirmed
+        you have received 2300 from 
+        James Muoki 
+        18th May 2022
+        safaricom transparent for you
+        '''
+print(msg)
+txt= """Holla! I am Bob Afwata
+        I come from Kiambu county
+         """
+print(txt)
 
+city = "Nairobi"
+print(city[:5])
+print(city[2:])
+print(city[-1:])
 
+f_name = "bob afwata"
+# .upper converts to uppercase
+print(f_name.upper())
 
+s_name = "KENNEDY MUOKI"
+#.lower() converts to lower case
+print(s_name.lower())
 
+#concatination - converting from one data type to another
+#int -> float
+#float-> int
+number = 6 
+print(str(number))
 
+x = 4
+print(float (x))
 
+y = 3.24
+print(int(y))
 
+f_name = "James"
 
+s_name = "Corden"
 
+full_name = f_name + s_name
+print(full_name)
 
-
-
-
-
-
-
+#the replace() method replaces a string  with another
+ 
 name = "Brett Cooper"
 
 print(name.replace('t','G'))
