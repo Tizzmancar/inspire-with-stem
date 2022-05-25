bb42ef234a8d03ef7f05a12ae3219a3dae8801d8
Tizzmancar <carsontheman51@gmail.com>
2022-05-25 09:33:51 +0300

    assignments

---
+++ assignment5.py
@@ -0,0 +1,13 @@
+#create a list of vehicles with the following
+# bmw , audi, toyota, mercedes, jeep
+#using if statement find a jeep in the list and convert it into uppercase
+
+# a list of vehicles
+vehicles= ['bmw', 'audi', 'toyota', 'mercedes','jeep']
+
+### using a for loop to print all vehicles
+for vehicle in vehicles:
+    if (vehicle==vehicles[4]):
+        print(vehicle.title())
+
+  
---
+++ lesson10.py
@@ -0,0 +1,7 @@
+#1/deskop/inspire with stem/lesson10.py
+
+######################################
+#       if statements
+#       Name : Carson Maina
+#       Date : 24/5/2022
+######################################
