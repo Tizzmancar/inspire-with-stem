04e1e1ff995a598fd4d4918383db276a7d7e4770
Tizzmancar <carsontheman51@gmail.com>
2022-05-27 11:43:05 +0300

    lessons

--- assignment6.py
+++ assignment6.py
@@ -1,11 +1,11 @@
-colours = ["red", "green" , "blue" , "purple"]
+## geometric progression
+a = int(input("enter first term "))
+r = int(input("enter common ratio "))
+n = int(input("enter number of terms "))
 
-# for colours in colours:
-#     if (colours =='red'):
-#         print(colours.upper())
+for i in range (1, n + 1):
+    n_term = a * r *(i-1)
+    print(n_term)
 
-i=0
-while i < len(colours):
-    if(colours[1]=='green'):
-        print(colours[1].upper())
-        i=i+ 1
+#sum
+s_n = float((a*(1-r**n) /1-r))
