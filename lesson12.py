f0a5fe953a0eccc39990effef6158d00d868f9b7
Tizzmancar <carsontheman51@gmail.com>
2022-05-30 10:36:57 +0300

    lesson

--- lesson12.py
+++ lesson12.py
@@ -1,11 +1,11 @@
 mary_fav_food = ['beef' , 'chicken' , ' vegetable']
 jane_fav_food = ['rice', 'ugali', 'potato']
-# foods =mary_fav_food , jane_fav_food
-# foods ={
-#     'mary':['beef' , 'chicken' , ' vegetable'],
-#     'jane':['rice', 'ugali', 'potato']}
-# print(foods)
-# print(type(foods))
+food =mary_fav_food , jane_fav_food
+foods ={
+    'mary':['beef' , 'chicken' , ' vegetable'],
+    'jane':['rice', 'ugali', 'potato']}
+print(foods)
+print(type(foods))
 
 names ={
     'mary' :{'gender':'female','phone_no':'45678','home':'nai'},
