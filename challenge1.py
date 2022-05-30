2624aac1a3880599b4889dd3109da735b26545cb
Tizzmancar <carsontheman51@gmail.com>
2022-05-30 11:14:05 +0300

    lessons

---
+++ challenge1.py
@@ -0,0 +1,13 @@
+## write a program to check if a number is divisible by 5 or 7
+# n = int(input("enter number "))
+# if (n%5==0) and (n%7==0):
+#     print(n, "is divisible by 7 and 5")
+# else:
+#     print(n, "is not divisible by 7 or 5")
+
+##  or
+# n=int(input("enter a number "))
+# if (n%7==0) and (n%5==0):
+#     print(f"The number {n} is divisible by 5 and 7")
+# else:
+#     print(f"the number {n} is not divisible by 5 or 7")
--- lesson12.py
+++ lesson12.py
@@ -1,13 +1,16 @@
 mary_fav_food = ['beef' , 'chicken' , ' vegetable']
 jane_fav_food = ['rice', 'ugali', 'potato']
-food =mary_fav_food , jane_fav_food
+#food =mary_fav_food , jane_fav_food
 foods ={
-    'mary':['beef' , 'chicken' , ' vegetable'],
+    'mary':['beef' , 'chicken' , 'vegetable'],
     'jane':['rice', 'ugali', 'potato']}
-print(foods)
-print(type(foods))
+# print(foods)
+# print(type(foods))
+for mary,jane in foods.items():
+    print(f"{mary}:{jane}")
 
-names ={
-    'mary' :{'gender':'female','phone_no':'45678','home':'nai'},
-    'jane' :{'gender':'female','phone_no':'98766','home':'thika'}}
-print(names)
+# names ={
+#     'mary' :{'female','45678','nai'},
+#     'jane' :{'female','98766','thika'}}
+# for mary,jane in names.items():
+#     print(f"{mary}:{jane}")
