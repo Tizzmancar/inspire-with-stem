d42298b2809c0c59ae0f8e84c7e0676ab91802ab
SamuelMaina <105628120+Tizzmancar@users.noreply.github.com>
2022-06-02 11:23:08 +0300

    lesson

---
+++ classes/employee.py
@@ -0,0 +1,8 @@
+class Employee:
+    def __init__(self,_name,_salary):
+        self.name = _name
+        self.salary = _salary
+    def sayHi(self):
+        print('hello my name is ' + str(self.name) + ' and i want ' + str(self.salary) + ' as my salary')
+employee1=Employee('Sam',987654)
+employee1.sayHi()
