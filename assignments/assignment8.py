85364108e9261f2765d1149aea1021ec89e427ca
SamuelMaina <105628120+Tizzmancar@users.noreply.github.com>
2022-06-04 16:32:04 +0300

    password generator

--- assignments/assignment8.py
+++ assignments/assignment8.py
@@ -1,8 +1,23 @@
+import random
+print("Welcome to our passsword generator")
+
 class Password:
     def __init__(self,user,password):
         self.user=user
         self.password=password
     def sayHi(self):
         print("username is " + self.user+ " password is " + self.password)
-password1 = Password('Sam','07yerya')
-password1.sayHi()
+
+
+print("Welcome to our passsword generator")
+character= str(input("enter characters to be used for password generation "))
+user = str(input("enter username "))
+num_password= int(input("How many passwords do you want to generate "))
+len_password= int(input("What is the length for your password "))
+print("\nHere are your passwords")
+for password in range (num_password):
+    password= ''
+    for c in range(len_password):
+        password+= random.choice(character)
+    password1=Password(user,password)
+    password1.sayHi()
