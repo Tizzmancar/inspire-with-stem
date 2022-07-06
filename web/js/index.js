a2ec56b840782c5a605f1952f519a9106104c306
Tizzmancar <carsontheman51@gmail.com>
2022-07-06 10:51:45 +0300

    web

---
+++ web/js/index.js
@@ -0,0 +1,13 @@
+let message = "Hello World"
+console.log(message)
+//this is a single line comment in java script
+/*
+this is a multi line comment in javascript
+*/
+let num1=134
+let num2=134
+sum = num1 + num2
+console.log(sum)
+function submitButton(){
+    alert("button has been clicked")
+}
